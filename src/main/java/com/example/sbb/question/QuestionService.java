ad3e8019ef4ed8753a04fe40f38264f896bac984
hadh93 <hoondog1993@gmail.com>
2023-01-12 00:44:42 -0600

    Detail information display page is implemented

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -2,8 +2,9 @@ package com.example.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import com.example.sbb.DataNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +14,15 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question is not found");
+        }
     }
 
 }
