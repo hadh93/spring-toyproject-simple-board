631532287715c66967536d044655a3d0cf5f107c
hadh93 <hoondog1993@gmail.com>
2023-01-18 13:33:48 -0600

    Posting answer functionality implemented

---
+++ src/main/java/com/example/sbb/answer/AnswerForm.java
@@ -0,0 +1,12 @@
+package com.example.sbb.answer;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerForm {
+    @NotEmpty(message = "You must enter content.")
+    private String content;
+}
