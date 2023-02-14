5410b102588b0fa51c17c6c188c56cd8b340276d
hadh93 <hoondog1993@gmail.com>
2023-02-13 22:16:26 -0600

    Answer is now modifiable

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -6,14 +6,13 @@ import com.example.sbb.user.SiteUser;
 import com.example.sbb.user.SiteUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -22,7 +21,7 @@ import java.security.Principal;
 @Controller
 public class AnswerController {
     private final QuestionService questionService;
-    private final AnswerService answerservice;
+    private final AnswerService answerService;
 
     private final SiteUserService userService;
 
@@ -37,9 +36,35 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerservice.create(question, answerForm.getContent(), siteUser);
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
 
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal){
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You do not have the permission to modify this content.");
+        }
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal){
+        if (bindingResult.hasErrors()){
+            return "answer_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You do not have the permission to modify this content.");
+        }
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 
 }
